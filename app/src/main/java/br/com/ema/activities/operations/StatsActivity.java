3260e968eaf5fef626c8ece3a1f6c0cd86cf672a
Samuel Zvir <samuelyuri@gmail.com>
2016-03-26 15:39:49 -0300

    show dialog when there's no user checked for statistics.

--- app/src/main/java/br/com/ema/activities/operations/StatsActivity.java
+++ app/src/main/java/br/com/ema/activities/operations/StatsActivity.java
@@ -3,9 +3,11 @@ package br.com.ema.activities.operations;
 
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
 
 import org.litepal.crud.DataSupport;
@@ -13,6 +15,8 @@ import org.litepal.crud.DataSupport;
 import java.util.List;
 
 import br.com.ema.R;
+import br.com.ema.activities.LoginActivity;
+import br.com.ema.dialogs.ChooseUser;
 import br.com.ema.entities.PlayStats;
 import br.com.ema.entities.Student;
 
@@ -39,37 +43,48 @@ public class StatsActivity extends Activity {
         Bundle bundle=intent.getExtras();
         this.student = (Student) bundle.getSerializable("student");
         loadBasicStats(student);
-
     }
 
     private void loadBasicStats(Student student){
-        int id = student.getId();
-        List<PlayStats> statsActivities = DataSupport.where("userId = ?",id+"").find(PlayStats.class);
-        int count = DataSupport.count(PlayStats.class);
-        Log.i(TAG,"Count: "+count);
-        for (PlayStats stats : statsActivities){
-            points += stats.getTotalPoints();
-            playedActivities++;
-            if(stats.getTotalPoints() > highestPointing){
-                highestPointing = (int) stats.getTotalPoints();
-            }
-            if(lowestPointing == 0){
-                lowestPointing = (int) stats.getTotalPoints();
-            }else if(stats.getTotalPoints() < lowestPointing){
-                lowestPointing = (int) stats.getTotalPoints();
-            }
+        if(student == null){
+            ChooseUser chooseUser = new ChooseUser();
+            FragmentManager fm = getFragmentManager();
+            chooseUser.show(fm,"Choose an user");
+        }else{
+            int id = student.getId();
+            List<PlayStats> statsActivities = DataSupport.where("userId = ?",id+"").find(PlayStats.class);
+            int count = DataSupport.count(PlayStats.class);
+            Log.i(TAG,"Count: "+count);
+            for (PlayStats stats : statsActivities){
+                points += stats.getTotalPoints();
+                playedActivities++;
+                if(stats.getTotalPoints() > highestPointing){
+                    highestPointing = (int) stats.getTotalPoints();
+                }
+                if(lowestPointing == 0){
+                    lowestPointing = (int) stats.getTotalPoints();
+                }else if(stats.getTotalPoints() < lowestPointing){
+                    lowestPointing = (int) stats.getTotalPoints();
+                }
 
-        } // for end.
+            } // for end.
 
-        name = (TextView) findViewById(R.id.nameForStats);
-        name.setText(student.getNickname());
-        pointsTextView = (TextView) findViewById(R.id.pointsTextValue);
-        pointsTextView.setText(String.valueOf(points));
-        playedActivitiesTextView = (TextView) findViewById(R.id.playedActivitiesTextValue);
-        playedActivitiesTextView.setText(String.valueOf(playedActivities));
-        highestPointingTextView = (TextView) findViewById(R.id.highestPointingTextValue);
-        highestPointingTextView.setText(String.valueOf(highestPointing));
-        lowestPointingTextView = (TextView) findViewById(R.id.lowestPointingTextValue);
-        lowestPointingTextView.setText(String.valueOf(lowestPointing));
+            name = (TextView) findViewById(R.id.nameForStats);
+            name.setText(student.getNickname());
+            pointsTextView = (TextView) findViewById(R.id.pointsTextValue);
+            pointsTextView.setText(String.valueOf(points));
+            playedActivitiesTextView = (TextView) findViewById(R.id.playedActivitiesTextValue);
+            playedActivitiesTextView.setText(String.valueOf(playedActivities));
+            highestPointingTextView = (TextView) findViewById(R.id.highestPointingTextValue);
+            highestPointingTextView.setText(String.valueOf(highestPointing));
+            lowestPointingTextView = (TextView) findViewById(R.id.lowestPointingTextValue);
+            lowestPointingTextView.setText(String.valueOf(lowestPointing));
+        }
+    }
+
+    public void goBack(View view){
+        Intent intent = new Intent(this, DataAnalysisActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 }
